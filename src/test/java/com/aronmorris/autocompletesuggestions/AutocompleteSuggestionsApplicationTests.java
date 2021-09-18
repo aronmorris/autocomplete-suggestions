c5bb72ec66c1ffb5f653054899c8f9a4c8e6c4c9
aronmorris <aron@undo.com>
2021-09-18 18:56:48 -0400

    adding items that should've been in first commit

---
+++ src/test/java/com/aronmorris/autocompletesuggestions/AutocompleteSuggestionsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.aronmorris.autocompletesuggestions;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AutocompleteSuggestionsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
